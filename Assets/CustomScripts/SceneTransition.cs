08a5d0c8d53a42f118be5e35f2cadf0b59d4fe42
Audrey Takenaga <takenaga@chapman.edu>
2021-05-21 16:33:44 -0700

    Scene Update
    Able to get fade transitions, but the camera movement is still there

--- Assets/CustomScripts/SceneTransition.cs
+++ Assets/CustomScripts/SceneTransition.cs
@@ -8,13 +8,39 @@ public class SceneTransition : MonoBehaviour
     public string SceneToLoad;
     public Vector2 PlayerPosition;
     public VectorValue PlayerStorage;
+    public GameObject FadeInPanel;
+    public GameObject FadeOutPanel;
+    public float FadeWait;
 
+    private void Awake()
+    {
+        if (FadeInPanel != null)
+        {
+            GameObject panel = Instantiate(FadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
             PlayerStorage.InitialValue = PlayerPosition;
-            SceneManager.LoadScene(SceneToLoad);
+            //SceneManager.LoadScene(SceneToLoad);
+            StartCoroutine(FadeCo());
+        }
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if (FadeOutPanel != null)
+        {
+            Instantiate(FadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(FadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(SceneToLoad);
+        while(!asyncOperation.isDone)
+        {
+            yield return null;
         }
     }
 }
